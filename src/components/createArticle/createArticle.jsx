6cd27acb6c0f06b1e9d7499456e45c6e8d4a6e81
Азамат Хасанов <hsm99@mail.ru>
2024-04-03 18:19:58 +0500

    редирект к статье

--- src/components/createArticle/createArticle.jsx
+++ src/components/createArticle/createArticle.jsx
@@ -1,5 +1,107 @@
-import { useState } from 'react'
-import { Redirect, useParams } from 'react-router-dom'
+// import { useState } from 'react'
+// import { Redirect, useParams } from 'react-router-dom'
+
+// import BlogService from '../../services/blog-services'
+// import Article from '../article/article'
+
+// const CreateArticle = ({ dataType }) => {
+//   const [tags, setTags] = useState([])
+//   const [createdArticle, setCreatedArticle] = useState(false)
+//   const { id } = useParams()
+//   const token = localStorage.getItem('token')
+//   const isLoggedIn = token ? true : false
+
+//   const { createArticle, updateArticle } = BlogService()
+
+//   // const onSubmit = async (data) => {
+//   //   const article = {
+//   //     article: {
+//   //       body: data.body,
+//   //       description: data.description,
+//   //       tagList: tags.filter((tag) => tag.replace(/\s/g, '') !== ''),
+//   //       title: data.title,
+//   //     },
+//   //   }
+
+//   //   const json = JSON.stringify(article)
+
+//   //   try {
+//   //     if (dataType === 'new-article') {
+//   //       const result = await createArticle(json, token)
+//   //       // console.log(result)
+//   //       // await createArticle(json, token)
+//   //     } else {
+//   //       await updateArticle(json, token, id)
+//   //     }
+//   //     setCreatedArticle(true)
+//   //   } catch (error) {
+//   //     console.log(error)
+//   //   }
+//   // }
+//   const onSubmit = async (data) => {
+//     const article = {
+//       article: {
+//         body: data.body,
+//         description: data.description,
+//         tagList: tags.filter((tag) => tag.replace(/\s/g, '') !== ''),
+//         title: data.title,
+//       },
+//     }
+
+//     const json = JSON.stringify(article)
+
+//     try {
+//       let result
+//       if (dataType === 'new-article') {
+//         result = await createArticle(json, token)
+//       } else {
+//         result = await updateArticle(json, token, id)
+//       }
+//       console.log(result)
+//       setCreatedArticle(true)
+//     } catch (error) {
+//       console.log(error)
+//     }
+//   }
+
+//   const handleAddTag = (elem) => {
+//     elem.preventDefault()
+//     setTags([...tags, ''])
+//   }
+
+//   const handleDeleteTag = (elem, index) => {
+//     elem.preventDefault()
+//     setTags((tags) => {
+//       return tags.filter((_, i) => i !== index)
+//     })
+//   }
+
+//   const handleTagChange = (index, value) => {
+//     const newTags = [...tags]
+//     newTags[index] = value
+//     setTags(newTags)
+//   }
+
+//   return (
+//     <>
+//       {createdArticle && <Redirect to="/" />}
+//       {!isLoggedIn && <Redirect to="/sign-in" />}
+//       <Article
+//         isLoggedIn={isLoggedIn}
+//         onSubmit={onSubmit}
+//         tags={tags}
+//         handleAddTag={handleAddTag}
+//         handleDeleteTag={handleDeleteTag}
+//         handleTagChange={handleTagChange}
+//         dataType={dataType}
+//       />
+//     </>
+//   )
+// }
+
+// export default CreateArticle
+import React, { useState } from 'react'
+import { Redirect, useParams, useHistory } from 'react-router-dom'
 
 import BlogService from '../../services/blog-services'
 import Article from '../article/article'
@@ -10,6 +112,7 @@ const CreateArticle = ({ dataType }) => {
   const { id } = useParams()
   const token = localStorage.getItem('token')
   const isLoggedIn = token ? true : false
+  const history = useHistory() // Получаем объект истории
 
   const { createArticle, updateArticle } = BlogService()
 
@@ -26,12 +129,17 @@ const CreateArticle = ({ dataType }) => {
     const json = JSON.stringify(article)
 
     try {
+      let result
       if (dataType === 'new-article') {
-        await createArticle(json, token)
+        result = await createArticle(json, token)
       } else {
-        await updateArticle(json, token, id)
+        result = await updateArticle(json, token, id)
       }
+      // console.log(result)
       setCreatedArticle(true)
+      if (result?.article?.slug) {
+        history.push(`blog/${result.article.slug}`)
+      }
     } catch (error) {
       console.log(error)
     }
@@ -57,7 +165,7 @@ const CreateArticle = ({ dataType }) => {
 
   return (
     <>
-      {createdArticle && <Redirect to="/" />}
+      {/* {createdArticle && <Redirect to="/" />} */}
       {!isLoggedIn && <Redirect to="/sign-in" />}
       <Article
         isLoggedIn={isLoggedIn}
